1a2ee3fbad7c4e2eb69d750d77c6b50f69fe2ddc
Yasir Khurshid <yasir.khurshid@get-in.de>
2020-04-13 23:10:44 +0200

    Implement country stats command

---
+++ commands/countryStatsCommand.js
@@ -0,0 +1,51 @@
+const prog = require('caporal');
+const logSymbols = require('log-symbols');
+const logUpdate = require('log-update');
+const chalk = require('chalk');
+const red = chalk.red;
+const spinner = require('./../utils/spinner');
+const statsApi = require('./../apis/stats');
+const table = require('./../utils/table');
+const countryValidator = require('./../utils/countryValidator');
+
+module.exports = {
+
+    init: (program) => {
+        return program
+            .command('country:stats', 'Display corona stats for countries')
+            .option('--name <country-name>', 'Show corona stats for <country-name>', prog.STRING)
+            .action(async (args, options, logger) => {
+                if (options.name) {
+                    spinner.start();
+                    const countryISO = countryValidator.validate(options.name);
+                    statsApi.getStatsByCountry(countryISO)
+                        .then(response => {
+                            spinner.stop();
+                            logUpdate(logSymbols.success, 'Fetched data successfully!');
+                            table.drawCountryStats(response.data);
+                            process.exit(0);
+                        })
+                        .catch((error) => {
+                            spinner.stop();
+                            logUpdate(red(logSymbols.error, 'Data fetching failed, please try again later!\n'), logSymbols.info, error);
+                            process.exit(1);
+                        });
+                    return;
+                }
+
+                spinner.start();
+                statsApi.getAllCountriesStats()
+                    .then(response => {
+                        spinner.stop();
+                        logUpdate(logSymbols.success, 'Fetched data successfully!');
+                        table.drawAllCountriesStats(response.data);
+                        process.exit(0);
+                    })
+                    .catch(error => {
+                        spinner.stop();
+                        logUpdate(red(logSymbols.error, 'Data fetching failed, please try again later!\n'), logSymbols.info, error);
+                        process.exit(1)
+                    });
+            });
+    }
+};
--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 const prog = require('caporal');
 const packageJson = require('./package.json');
 const globalStatsCommand = require('./commands/globalStatsCommand');
+const countryStatsCommand = require('./commands/countryStatsCommand');
 
 let program = prog.version(`${packageJson.version} by ${packageJson.author}`);
 program = globalStatsCommand.init(program);
+program = countryStatsCommand.init(program);
 
 prog.parse(process.argv);
